c06dc870946912ec420c061291fe8360b2d541d9
Rokas Stankevičius <rokasstankevicius3@gmail.com>
2022-07-24 22:07:47 +0300

    my-app

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+    const footer = (
+        <footer class="footer">
+          <small>2022 Rokas Stankevičius developement. All rights reserved.</small>
+        </footer>
+    )
+    return(
+      <div>
+        {footer}
+      </div>
+    )
+  }
+
+export default Footer;
---
+++ src/components/MainContent.js
@@ -0,0 +1,20 @@
+const MainContent = () => {
+    const mainContent = (
+      <div className="main-content">
+        <h1>Reasons I'm excited to learn React!</h1>
+        <ol>
+          <li>It's a popular library.</li>
+          <li>It's fun to use.</li>
+        </ol>
+      </div>
+    )
+    return(
+      <div>
+        {mainContent}
+      </div>
+    )
+  }
+  
+  
+  export default MainContent;
+  
